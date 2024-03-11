1eaba4680be3aed9cf72f2cec0ede97c55e9c4db
whelixw <andrerosenj@gmail.com>
2024-03-11 15:18:19 +0100

    fixed the hard-coded path, so now paths can be read from vg either by provided path or inferred

--- Cutoff_Node.py
+++ Cutoff_Node.py
@@ -1,6 +1,8 @@
+#todo: use arparse instead of sys.argv to make script in line with VG iterative
 from bdsg.bdsg import ODGI
-import subprocess 
+import subprocess
 import sys
+from pathlib import Path
 
 def get_number_of_paths_per_node(x):
     node_paths_tuple.append((graph.get_id(x), graph.get_step_count(x)))
@@ -11,17 +13,34 @@ def get_path_support(graph, node_id):
     graph.for_each_step_on_handle(graph.get_handle(node_id), lambda x: paths.append(graph.get_path_name(graph.get_path_handle_of_step(x))) or True)
     return paths
 
+if len(sys.argv) < 2:
+    print("Usage: script.py <path_to_odgi_file> [path_to_vg_file]")
+    sys.exit(1)
+
 graph = ODGI()
-graph.deserialize(sys.argv[1])
-paths = subprocess.run(f"vg paths -v VG_output/Suina_output.vg -L | wc -l", shell=True, capture_output=True, text=True)
+odgi_file_path = Path(sys.argv[1])
+graph.deserialize(str(odgi_file_path))
+base_path = odgi_file_path.parent
+base_name = odgi_file_path.stem
+
+# Infer VG file path or use the provided second argument
+if len(sys.argv) == 3:
+    vg_file_path = Path(sys.argv[2])
+else:
+    vg_file_path = base_path / f"{base_name}.vg"
+
+print(vg_file_path)
+print(base_name)
+cmd = f"vg paths -v {vg_file_path} -L | wc -l"
+paths = subprocess.run(cmd, shell=True, capture_output=True, text=True)
 number_of_paths = int(paths.stdout.strip())
-print(f"The number of paths in the graphs is:", number_of_paths)
+print(f"The number of paths in the graphs is: {number_of_paths}")
 
 # For each node get the number of paths
 node_paths_tuple = []
 graph.for_each_handle(lambda x: get_number_of_paths_per_node(x))
 
-# Get the nodes through which all paths go through 
+# Get the nodes through which all paths go through
 nodes_to_consider = []
 for t in node_paths_tuple:
     if t[1] == number_of_paths:
@@ -29,7 +48,7 @@ for t in node_paths_tuple:
 
 # or find the next best node
 while nodes_to_consider == []:
-    number_of_paths = int(number_of_paths)-1
+    number_of_paths -= 1
     for t in node_paths_tuple:
         if t[1] == number_of_paths:
             nodes_to_consider.append(t[0])
@@ -53,5 +72,3 @@ print("Nodes with maximum length of", max_length, "bp: ")
 for item in node_length_dict.items():
     if item[1] == max_length:
         print(item[0])
-
-
