88f44a94b7cb74d1626db5148cb1666a42e1194a
whelixw <andrerosenj@gmail.com>
2024-06-26 18:58:41 +0200

    mades script save to .fasta instead of .fa, made script rename fastas to first word in header (option)

--- request_fastas.py
+++ request_fastas.py
@@ -23,6 +23,12 @@ def fetch_fasta_from_ena(accession):
         return response.text
     return None
 
+def extract_name_from_header(fasta_data):
+    """Extract the first word from the FASTA header (assumed to be after '>')."""
+    first_line = fasta_data.partition('\n')[0]
+    first_word = first_line.split()[0][1:]  # Assumes there's a word after '>'
+    return first_word
+
 def save_fasta(fasta, filename):
     """Save the fetched FASTA sequence to a file."""
     with open(filename, 'w') as file:
@@ -34,31 +40,35 @@ def file_exists(filename, directory):
     return os.path.isfile(os.path.join(directory, filename)) if directory else os.path.isfile(filename)
 
 def main(args):
-    # Set the directory for saving FASTA files
     directory = args.directory if args.directory else "."
-    # Set email
     email = args.email if args.email else "default_email@example.com"
 
     for accession in args.accessions:
-        filename = f"{accession}.fasta"
-        full_path = os.path.join(directory, filename)
+        fasta = fetch_fasta_from_ncbi(accession, email)
+        if not fasta:
+            print(f"No data from NCBI for {accession}, trying ENA...")
+            fasta = fetch_fasta_from_ena(accession)
+        
+        if fasta:
+            new_filename = f"{accession}.fasta"
+            if args.rename:
+                new_accession = extract_name_from_header(fasta)
+                new_filename = f"{new_accession}.fasta"
+            full_path = os.path.join(directory, new_filename)
 
-        if not file_exists(filename, directory):
-            fasta = fetch_fasta_from_ncbi(accession, email)
-            if not fasta:
-                print(f"No data from NCBI for {accession}, trying ENA...")
-                fasta = fetch_fasta_from_ena(accession)
-            if fasta:
+            if not file_exists(new_filename, directory):
                 save_fasta(fasta, full_path)
             else:
-                print(f"Failed to retrieve FASTA for {accession}")
+                print(f"File {new_filename} already exists. Skipping download.")
         else:
-            print(f"File {filename} already exists. Skipping download.")
+            print(f"Failed to retrieve FASTA for {accession}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fetch FASTA sequences from NCBI and ENA")
     parser.add_argument('accessions', nargs='+', help='List of accession numbers')
     parser.add_argument('--directory', '-d', help='Directory to save FASTA files', required=False)
     parser.add_argument('--email', '-e', help='Email for NCBI requests', required=False)
+    parser.add_argument('--rename', action='store_true', help='Rename output files based on the first word in the FASTA header')
     args = parser.parse_args()
     main(args)
+
